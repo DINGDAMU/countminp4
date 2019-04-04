c27a7203458cc9c63713fde69c3bbdb6b24c4da7
DINGDAMU <dingdamu@gmail.com>
2019-04-04 13:17:09 +0200

    remove checkout

--- start.sh
+++ start.sh
@@ -8,11 +8,11 @@ set -e
  
 # 设置相关路径和版本变量
 P4_HOME=$HOME/P4
-BMV2_COMMIT="7e25eeb19d01eee1a8e982dc7ee90ee438c10a05"
-PI_COMMIT="219b3d67299ec09b49f433d7341049256ab5f512"
-P4C_COMMIT="48a57a6ae4f96961b74bd13f6bdeac5add7bb815"
-PROTOBUF_COMMIT="v3.2.0"
-GRPC_COMMIT="v1.3.2"
+#BMV2_COMMIT="7e25eeb19d01eee1a8e982dc7ee90ee438c10a05"
+#PI_COMMIT="219b3d67299ec09b49f433d7341049256ab5f512"
+#P4C_COMMIT="48a57a6ae4f96961b74bd13f6bdeac5add7bb815"
+#PROTOBUF_COMMIT="v3.2.0"
+#GRPC_COMMIT="v1.3.2"
 #获得cpu核数，与某些软件的编译选项相关
 NUM_CORES=`grep -c ^processor /proc/cpuinfo`
  
@@ -26,7 +26,7 @@ cd ..
 # 安装Protobuf
 git clone https://github.com/google/protobuf.git
 cd protobuf
-git checkout ${PROTOBUF_COMMIT}
+#git checkout ${PROTOBUF_COMMIT}
 export CFLAGS="-Os"
 export CXXFLAGS="-Os"
 export LDFLAGS="-Wl,-s"
@@ -44,7 +44,7 @@ cd ../..
 # 安装gRPC
 git clone https://github.com/grpc/grpc.git
 cd grpc
-git checkout ${GRPC_COMMIT}
+#git checkout ${GRPC_COMMIT}
 git submodule update --init --recursive
 export LDFLAGS="-Wl,-s"
 make -j${NUM_CORES}
@@ -58,7 +58,7 @@ sudo pip install grpcio
 # 安装BMv2的依赖，下面PI编译时会用到。
 git clone https://github.com/p4lang/behavioral-model.git
 cd behavioral-model
-git checkout ${BMV2_COMMIT}
+#git checkout ${BMV2_COMMIT}
 # From bmv2's install_deps.sh, we can skip apt-get install.
 # Nanomsg is required by p4runtime, p4runtime is needed by BMv2...
 tmpdir=`mktemp -d -p .`
@@ -74,7 +74,7 @@ cd ..
 # PI/P4Runtime
 git clone https://github.com/p4lang/PI.git
 cd PI
-git checkout ${PI_COMMIT}
+#git checkout ${PI_COMMIT}
 git submodule update --init --recursive
 ./autogen.sh
 ./configure --with-proto
@@ -104,7 +104,7 @@ cd ..
 # 安装P4C，省去了check步骤（太费时间了）
 git clone https://github.com/p4lang/p4c
 cd p4c
-git checkout ${P4C_COMMIT}
+#git checkout ${P4C_COMMIT}
 git submodule update --init --recursive
 mkdir -p build
 cd build
